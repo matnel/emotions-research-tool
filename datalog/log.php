d18c6303245db3b6d25613397f8d0b977d2c6da0
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2011-03-20 10:10:13 +0200

    initial work for mysql connection

--- datalog/log.php
+++ datalog/log.php
@@ -1,7 +1,14 @@
 <?php
+	include('settings.php');
 
 	/* determins if done to a file or to sql */
 	$debug = isset( $_GET['debug'] ) ?  $_GET['debug'] : false;
+
+	/* initialize sql if needed. must be on for mysql_real_escape_string to work */
+	if( !$debug ) {
+		mysql_connect( $MYSQL_SERVER, $MYSQL_USER , $MYSQL_PASSWORD );
+		mysql_selectdb( $MYSQL_DATABASE );
+	}
 
 	/* function for cleaning data, i.e. making it save to be written */
 	function clean_data( $data ) {
@@ -21,6 +28,14 @@
 		fwrite( $file , $coordx.'|'.$coordy.'|'.$data ."\n" );
 		fclose( $file );
 	} else {
-
+		var_dump( $data );
+		$data = explode( '|', $data );
+		var_dump( $data );
+		$query = 'INSERT INTO '.$MYSQL_TABLE.' (x,y,company,data) VALUES ('.
+			$coordx.','.$coordy.',\''.$data[0].'\',\''.$data[1].'\''.
+			')';
+		echo $query;
+		mysql_query( $query );
+		mysql_close();
 	}
 ?>
